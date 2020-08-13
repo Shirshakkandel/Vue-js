e87596e688b67511a2f78a9f8c316a3ac764a2f7
SHIRSHAK\shirshak <shirshakkandel@gmail.com>
2020-08-13 12:37:29 +0545

    5)even handling

---
+++ main.js
@@ -0,0 +1,39 @@
+
+ var app = new Vue({
+   el:'#app',
+   data:{
+      product: "Socks",
+      image:"./image/vmSocks-green-onWhite.jpg",
+      inStock :false,
+      details:["80% cotton","20% polyester", "Gender-neutral"],
+
+      variants: [
+         {
+            variantId:1,
+            variantColor:"green",
+            variantImage:"./image/vmSocks-green-onWhite.jpg"
+         },
+         {
+            variantId:2,
+            variantColor:"blue",
+            variantImage:"./image/vmSocks-blue-onWhite.jpg"
+         }
+
+      ],
+
+      cart:0
+   },
+
+   methods:{
+      addToCart(){
+         this.cart +=1
+      },
+
+      updateProduct(variantImage){
+         this.image =variantImage
+      }
+   }
+
+
+     
+   });
